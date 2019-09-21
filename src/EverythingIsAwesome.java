35af909335efca7053256be52f09f718211e06e0
League Student <league@iMac-10.attlocal.net>
2019-09-21 11:30:16 -0700

    module 0 finished

---
+++ src/EverythingIsAwesome.java
@@ -0,0 +1,7 @@
+import javax.swing.JOptionPane;
+
+public class EverythingIsAwesome {
+public static void main(String[] args) {
+	String answer=JOptionPane.showInputDialog("what would you like?");
+}
+}
---
+++ src/LastSummer.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+
+public class LastSummer {
+public static void main(String[] args) {
+	String name=JOptionPane.showInputDialog("what is your name?");
+	JOptionPane.showMessageDialog(null, "I know that you went outside last summer, "+name+",muhahaha" );
+}
+}
